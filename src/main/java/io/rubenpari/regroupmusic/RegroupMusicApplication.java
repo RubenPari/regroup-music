8b5970ff7b86c800ccc94c8a1b79c3bea2fd68ef
RubenPari <rubenpari.developer@gmail.com>
2024-11-16 16:00:31 +0100

    Initial commit

---
+++ src/main/java/io/rubenpari/regroupmusic/RegroupMusicApplication.java
@@ -0,0 +1,13 @@
+package io.rubenpari.regroupmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegroupMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegroupMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/rubenpari/regroupmusic/RegroupMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rubenpari.regroupmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegroupMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
