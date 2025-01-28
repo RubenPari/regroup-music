3696ce88291fe5ae5418b1cf6aeca0c6bc6cfe3d
RubenPari <rubenpari.developer@gmail.com>
2025-01-28 18:45:01 +0100

    added enum ArtistsSummarySortType

---
+++ src/models/artistsSummarySortType.ts
@@ -0,0 +1,8 @@
+enum ArtistsSummarySortType {
+  MostToLeastCounted = 'most_to_least_counted',
+  LeastToMostCounted = 'least_to_most_counted',
+  Alphabetical = 'alphabetical',
+  ReverseAlphabetical = 'reverse_alphabetical',
+}
+
+export default ArtistsSummarySortType;
