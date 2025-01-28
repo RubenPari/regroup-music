ef209958682f06c1808c5fcd0e1e34c94e93b3b8
RubenPari <rubenpari.developer@gmail.com>
2025-01-28 18:42:52 +0100

    added getAllUserTracks track.service method

--- src/modules/track/track.service.ts
+++ src/modules/track/track.service.ts
@@ -6,4 +6,29 @@ import { SpotifyService } from '../spotify/spotify.service';
 @Injectable()
 export class TrackService {
   constructor(private readonly spotifyService: SpotifyService) { }
+
+  async getAllUserTracks(): Promise<Array<SpotifyApi.SavedTrackObject>> {
+    const limit = 50;
+    let offset = 0;
+    let totalUserTracks: Array<SpotifyApi.SavedTrackObject> = [];
+
+    for (let i = 0; i < limit; i++) {
+      const tracksPage = await this.spotifyService
+        .getClient()
+        .getMySavedTracks({
+          limit,
+          offset,
+        });
+
+      if (tracksPage.body.items.length === 0) {
+        break;
+      }
+
+      totalUserTracks = totalUserTracks.concat(tracksPage.body.items);
+
+      offset += limit;
+    }
+
+    return totalUserTracks;
+  }
 }
