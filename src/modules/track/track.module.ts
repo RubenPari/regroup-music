2b34a01fc0464350de6999041db5178adaccc74a
RubenPari <rubenpari.developer@gmail.com>
2025-01-28 18:46:32 +0100

    updated track.module.ts

--- src/modules/track/track.module.ts
+++ src/modules/track/track.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TrackController } from './track.controller';
+import { TrackService } from './track.service';
+import { SpotifyModule } from '../spotify/spotify.module';
 
 @Module({
   controllers: [TrackController],
+  providers: [TrackService],
+  imports: [SpotifyModule],
 })
 export class TrackModule {}
