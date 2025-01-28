2d57abd04e0f1c13896008ff981311f4a428588e
RubenPari <rubenpari.developer@gmail.com>
2025-01-28 18:43:42 +0100

    added getArtistsSummary track.service method

--- src/modules/track/track.service.ts
+++ src/modules/track/track.service.ts
@@ -31,4 +31,32 @@ export class TrackService {
 
     return totalUserTracks;
   }
+
+  /**
+   * create an array of objects where each object has:
+   * - artist name
+   * - artist id
+   * - track number
+   * @param totalUserTracks
+   */
+  getArtistsSummary(
+    totalUserTracks: Array<SpotifyApi.SavedTrackObject>,
+  ): ArtistSummary[] {
+    return totalUserTracks.reduce<ArtistSummary[]>((acc, track) => {
+      const mainArtist = track.track.artists[0];
+
+      if (!acc[mainArtist.id]) {
+        acc[mainArtist.id] = {
+          name: mainArtist.name,
+          id: mainArtist.id,
+          count: 1,
+        };
+      } else {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        acc[mainArtist.id].count++;
+      }
+
+      return acc;
+    }, {} as ArtistSummary[]);
+  }
 }
