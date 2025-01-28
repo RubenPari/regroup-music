a922832e6c8b39891cc562402b998d32c48513bd
RubenPari <rubenpari.developer@gmail.com>
2025-01-28 20:14:37 +0100

    changed session stored type with Redis

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import session from 'express-session';
+import { RedisStore } from 'connect-redis';
+import { createClient } from 'redis';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const redisClient = createClient({
+    url: process.env.REDIS_URL!,
+  });
+
+  redisClient.connect().catch(console.error);
+
+  const redisStore = new RedisStore({
+    client: redisClient,
+  });
+
   app.use(
     session({
-      secret: process.env.SESSION_SECRET!,
+      store: redisStore,
       resave: false,
       saveUninitialized: false,
+      secret: process.env.SESSION_SECRET!,
+      cookie: { secure: false }, // TODO: set to true in production
     }),
   );
 
