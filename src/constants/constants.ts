a88c7516975e37812c8ace2edf7daa24c285909b
RubenPari <rubenpari.developer@gmail.com>
2025-02-15 16:00:38 +0100

    added class for costants

---
+++ src/constants/constants.ts
@@ -0,0 +1,8 @@
+class Constants {
+  public static PLAYLIST_NAME_WITH_MINOR_SONGS: string = 'Minor Songs';
+  public static MAX_COUNT_MINOR_SONGS: number = 5;
+  public static OFFSET: number = 0;
+  public static LIMIT_INSERT_PLAYLIST_TRACKS: number = 100;
+}
+
+export default Constants;
